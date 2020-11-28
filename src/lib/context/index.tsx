a403b2ff9a244eaf4527f73686e43e9feb848371
filoscoder <sondaniel.88@gmail.com>
2020-11-28 17:42:45 +0100

    Updated UserContext

--- src/lib/context/index.tsx
+++ src/lib/context/index.tsx
@@ -1,11 +1,16 @@
 import { createContext } from 'react';
 
 type UserContextType = {
-    token: undefined | string,
+    token: string,
+    userId: string,
 }
 
+const token = localStorage.getItem("ig_token") || process.env.REACT_APP_IG_TESTER_TOKEN || '';
+const userId = localStorage.getItem("ig_user_id") || '';
+
 const UserContext = createContext<UserContextType>({
-    token: process.env.IG_TOKEN,
+    token: token,
+    userId: userId
 });
 
 export default UserContext;
