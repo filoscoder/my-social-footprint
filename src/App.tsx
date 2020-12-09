d01d5d14107a53ed616dc93bc92c17a9ed0db476
filoscoder <sondaniel.88@gmail.com>
2020-12-09 18:15:32 +0100

    Updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,78 @@
-import { Route, Switch } from 'react-router-dom';
+import {
+    FacebookFilled,
+    InstagramFilled,
+    YoutubeFilled,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import { Link, Switch } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
 
-import AuthPage from "./pages/AuthPage";
-import AuthRoute from "./routes/AuthRoute";
-import { FlexContainer } from './components/containers/flexContainer';
-import MainPage from "./pages/MainPage";
-import React from "react";
+import AuthPage from './pages/AuthPage';
+import AuthRoute from './routes/AuthRoute';
+import { Context } from './lib/store';
+import MainPage from './pages/MainPage';
+import NAuthRoute from './routes/NAuthRoute';
+import styled from 'styled-components';
+
+const { Header } = Layout;
+
 
 function App() {
+    const [state, dispatch] = useContext(Context);
+    const [selectedTab, setSelectedTab] = useState(state.defaultSocial);
+
+    const onSelectTab = ({ item, key, keyPath, selectedKeys, domEvent }: any) => {
+        setSelectedTab(key);
+        dispatch({ type: 'SET_SOCIAL', social: key });
+    }
 
     return (
-        <MainPage />
+        <Layout style={{ height: "-webkit-fill-available" }}>
+            <Header style={{ padding: 0 }}>
+                <LeftBlock>
+                    <LogoBlock href={`/${selectedTab}`}>
+                        {selectedTab}
+                    </LogoBlock>
+                    <Menu mode="horizontal" defaultSelectedKeys={[state.defaultSocial]} theme={state.theme}
+                        onSelect={onSelectTab}
+                    >
+                        <Menu.Item key="facebook">
+                            <Link to="/facebook">
+                                <FacebookFilled style={{ margin: 0 }} />
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="instagram">
+                            <Link to="/instagram">
+                                <InstagramFilled style={{ margin: 0 }} />
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="youtube">
+                            <Link to="/youtube">
+                                <YoutubeFilled style={{ margin: 0 }} />
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </LeftBlock>
+            </Header>
+            <Switch>
+                <AuthRoute exact path={`/${selectedTab}`} component={MainPage} social={selectedTab} />
+                <NAuthRoute exact path={`/${selectedTab}/signin`} component={AuthPage} social={state.social} />
+            </Switch>
+        </Layout>
     );
 }
+
+const LeftBlock = styled.div`
+display: flex;
+justify-content: space-between;
+margin: 0 10px;
+`;
+const LogoBlock = styled.a`
+width: 60px;
+display: flex;
+justify-content: center;
+background-color: lightgray;
+`;
+
 
 export default App;
