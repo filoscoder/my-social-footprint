85d4fa2a90f8c7a5c8af47e113ddd6d2d71926ad
filoscoder <sondaniel.88@gmail.com>
2020-12-18 00:04:03 +0100

    Created StyledText

---
+++ src/components/common/StyledText.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import palette from '../../lib/styles/palette'
+import styled from 'styled-components';
+
+type FontWeightType = "normal" | "bold" | "bolder" | "lighter" | "initial" | "inherit";
+interface StyledTextProps extends React.HTMLProps<HTMLFontElement> {
+    color?: string,
+    fontSize?: number | string,
+    fontWeight?: number | FontWeightType,
+};
+
+const StyledText: React.FC<StyledTextProps> = (props) => {
+    const { color = palette.primaryMain, fontSize = 'inherit', fontWeight = 'inherit', children, ...rest } = props;
+    const htmlProps = rest as any;
+    console.log(fontWeight)
+    return (
+        <StyledTextBody color={color} size={fontSize} weight={fontWeight} {...htmlProps}>
+            {children}
+        </StyledTextBody >
+    );
+}
+
+const StyledTextBody = styled.p<{ color: string, size: number | string, weight: number | FontWeightType }>`
+    margin: 0;
+    padding: 0;
+    color: ${(props) => props.color};
+    font-size: ${(props) => (typeof props.size === 'number') ? props.size + "px" : props.size};
+    font-weight: ${(props) => props.weight};
+`
+
+export default StyledText;
