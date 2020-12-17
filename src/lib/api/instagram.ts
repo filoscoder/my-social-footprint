745dd1bebbc7ac0dec3cc4a7d3759b6b0d6a5e74
filoscoder <sondaniel.88@gmail.com>
2020-12-18 00:05:28 +0100

    Updated ig api

--- src/lib/api/instagram.ts
+++ src/lib/api/instagram.ts
@@ -1,126 +1,102 @@
 import axios, { AxiosResponse } from "axios";
 
+import { FbPageInfoType } from "../../pages/facebook/types";
+import { IgAccountType } from "../../pages/instagram/types";
 import { openNotification } from "../utils";
 
-const AppId = process.env.REACT_APP_IG_CLIENT_ID || "";
-const AppSecret = process.env.REACT_APP_IG_CLIENT_SECRET || "";
-const redirectUri =
-  "https://localhost:3000/" || process.env.REACT_APP_REDIRECT_URL;
-const tokenUrl = process.env.REACT_APP_IG_TOKEN_URL || "";
-const authUrl = process.env.REACT_APP_IG_AUTH_URL || "";
-const apiUrl = process.env.REACT_APP_IG_API_URL || "";
+const ApiUrl = process.env.REACT_APP_FB_API_URL ?? "";
+const SdkVer = process.env.REACT_APP_FB_SDK_VER ?? "";
 
-export const getAccessToken = async () => {
-  const code = new URLSearchParams(window.location.search).get("code") || "";
-  if (code === "") {
-    return;
-  }
-
+export const getIgBusinessAcc = async () => {
   try {
-    const bodyFormData = new FormData();
-    bodyFormData.append("client_id", AppId);
-    bodyFormData.append("client_secret", AppSecret);
-    bodyFormData.append("grant_type", "authorization_code");
-    bodyFormData.append("redirect_uri", `${redirectUri}instagram/`);
-    bodyFormData.append("code", code);
+    const igBusinessAccounts = localStorage.getItem("igBusinessAccounts");
 
-    const response = await axios.post(tokenUrl, bodyFormData);
-
-    if (response.status !== 200) {
-      openNotification(
-        "error",
-        `Error`,
-        "Something went wrong. \n Please, try again"
+    if (!igBusinessAccounts) {
+      const storeFbPages: Array<FbPageInfoType> = JSON.parse(
+        localStorage.getItem("fbPages") || ""
       );
-      return null;
+
+      if (storeFbPages) {
+        const igBusinessData = await Promise.all(
+          storeFbPages.map(async (page) => {
+            let { data } = await axios.get(`${ApiUrl}/${SdkVer}/${page.id}`, {
+              params: {
+                fields: "instagram_business_account,name",
+                access_token: page.access_token,
+              },
+            });
+            return {
+              id: data.instagram_business_account.id,
+              name: data.name,
+              access_token: page.access_token,
+            };
+          })
+        );
+
+        localStorage.setItem(
+          "igBusinessAccounts",
+          JSON.stringify(igBusinessData)
+        );
+
+        return igBusinessData;
+      } else {
+        return;
+      }
+    } else {
+      return JSON.parse(igBusinessAccounts);
     }
-
-    const { id, username } = await getMe(response.data.access_token);
-
-    sessionStorage.setItem("instagram_token", response.data.access_token);
-    sessionStorage.setItem("instagram_userId", id);
-    sessionStorage.setItem("instagram_username", username);
-
-    openNotification("success", "Logged in", `Welcome back ${username}!`);
-
-    return { token: response.data.access_token, userId: response.data.user_id };
   } catch (error) {
     openNotification(
       "error",
-      `Error`,
-      "Something went wrong. \n Please, try again"
+      "No account",
+      "You need to connect a Instagram business account"
     );
-    console.log("[getAccessToken] Error: ", error);
-  }
-};
-
-export const getAuthCode = async () => {
-  try {
-    const response = await axios.get(authUrl, {
-      params: {
-        client_id: AppId,
-        redirect_uri: `${redirectUri}instagram/`,
-        scope: "user_profile,user_media",
-        response_type: "code",
-      },
-    });
-
-    if (response.status !== 200) {
-      return;
-    }
-
-    const storageToken = sessionStorage.getItem("instagram_token");
-    const storageUserId = sessionStorage.getItem("instagram_userId");
-
-    if (storageToken || storageUserId) {
-      sessionStorage.removeItem("instagram_token");
-      sessionStorage.removeItem("instagram_userId");
-    }
-
-    return (window.location.href = response.request.responseURL);
-  } catch (error) {
-    console.log("[getAccessToken] Error: ", error);
+    console.log("[getIgBusinessAcc] Error: ", error);
   }
 };
 
 export const getMe = async (
-  token: string,
-  fields = "account_type,id,username,media_count"
+  account: IgAccountType,
+  fields: string = "biography,id,ig_id,username,follows_count,followers_count,media_count,name,profile_picture_url"
 ) => {
   try {
-    const response = await axios.get(`${apiUrl}/me`, {
+    console.log(account);
+    const response: any = await axios.get(`${ApiUrl}/${SdkVer}/${account.id}`, {
       params: {
-        fields: fields,
-        access_token: token,
+        fields,
+        access_token: account.access_token,
       },
     });
 
     if (response.status !== 200) {
       return;
     }
+    console.log(response);
     return response.data;
   } catch (error) {
     console.log("[getMe] Error: ", error);
   }
 };
 
-export const getMedia = async (
+export const getMedias = async (
+  account: IgAccountType,
   fields: string = "id,username,caption,media_type,media_url,thumbnail_url,timestamp"
 ) => {
   try {
-    const storageToken = sessionStorage.getItem("instagram_token");
-    const storageUserId = sessionStorage.getItem("instagram_userId");
-
-    const response: any = await axios.get(`${apiUrl}/${storageUserId}/media`, {
-      params: {
-        fields: fields,
-        access_token: storageToken,
-      },
-    });
+    console.log(account);
+    const response: any = await axios.get(
+      `${ApiUrl}/${SdkVer}/${account.id}/media`,
+      {
+        params: {
+          access_token: account.access_token,
+        },
+      }
+    );
 
     if (response.status !== 200) {
       return;
     }
+    console.log(response);
     return {
       data: response.data.data,
       next: response.data.paging.next,
@@ -130,7 +106,7 @@ export const getMedia = async (
   }
 };
 
-export const getMoreMedia = async (nextUrl: string) => {
+export const getMoreMedias = async (nextUrl: string) => {
   try {
     const response: any = await axios.get(nextUrl);
 
