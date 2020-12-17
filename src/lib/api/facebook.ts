603f09458c0079f3e3b646fdb490290995b44ffd
filoscoder <sondaniel.88@gmail.com>
2020-12-18 00:05:35 +0100

    Fixed fb api

--- src/lib/api/facebook.ts
+++ src/lib/api/facebook.ts
@@ -1,55 +1,102 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-const AppId = process.env.REACT_APP_ID || "";
-const authUrl = process.env.REACT_APP_FB_AUTH_URL || "";
-const redirectUri =
-  "https://localhost:3000" || process.env.REACT_APP_REDIRECT_URL;
+import { LoginStatusType } from "../../pages/facebook/types";
+
+const ApiUrl = process.env.REACT_APP_FB_API_URL ?? "";
+const SdkVer = process.env.REACT_APP_FB_SDK_VER ?? "";
 
 export const login = async () => {
-  // login with facebook then authenticate with the API to get a JWT auth token
+  localStorage.removeItem("fbLoginStatus");
+  const response: LoginStatusType = await new Promise(window.FB.login);
+  if (!response.authResponse) return;
+  if (response.status === "connected") {
+    localStorage.setItem("fbLoginStatus", JSON.stringify(response));
+    console.log(response);
 
-  const { authResponse } = await new Promise(window.FB.login);
-  if (!authResponse) return;
-
-  window.FB.api("/me", function (response: any) {
-    console.log("Good to see you, " + response.name + ".");
-  });
-
-  const { accessToken, userID, expiresIn, graphDomain } = authResponse;
-
-  console.log(authResponse);
-
-  //   await apiAuthenticate(authResponse.accessToken);
-
-  // get return url from location state or default to home page
-  //   const { from } = history.location.state || { from: { pathname: "/" } };
-  //   history.push(from);
+    return window.location.reload();
+  }
 };
 
-// export const apiAuthenticate = async (accessToken: string) => {
-//     // authenticate with the api using a facebook access token,
-//     // on success the api returns an account object with a JWT auth token
-//     const response = await axios.post(`${baseUrl}/authenticate`, { accessToken });
-//     const account = response.data;
-//     accountSubject.next(account);
-//     startAuthenticateTimer();
-//     return account;
-// }
-export const checkLoginState = async () => {
-  const { authResponse } = await new Promise(window.FB.getLoginStatus);
-  if (!authResponse) return;
-  const { accessToken, userID, expiresIn, graphDomain } = authResponse;
+export const checkFbLoginState = async () => {
+  const storeFbLoginStatus = localStorage.getItem("fbLoginStatus");
+  if (!storeFbLoginStatus) {
+    const response: LoginStatusType = await new Promise(
+      window.FB.getLoginStatus
+    );
+    localStorage.setItem("fbLoginStatus", JSON.stringify(response));
+    return response;
+  } else {
+    return JSON.parse(storeFbLoginStatus);
+  }
+};
 
-  console.log(authResponse);
-  return authResponse;
+export const getMe = async () => {
+  try {
+    const storeFbLoginStatus = JSON.parse(
+      localStorage.getItem("fbLoginStatus") || ""
+    );
+
+    const response: AxiosResponse = await axios.get(`${ApiUrl}/${SdkVer}/me`, {
+      params: {
+        access_token: storeFbLoginStatus.authResponse.accessToken,
+      },
+    });
+
+    if (response.status !== 200) {
+      return;
+    }
+
+    localStorage.setItem("fbMe", JSON.stringify(response.data));
+
+    return response.data;
+  } catch (error) {
+    console.log("[getMe] Error: ", error);
+  }
+};
+
+export const getFbPages = async () => {
+  try {
+    const storeFbPages = localStorage.getItem("fbPages");
+    if (!storeFbPages) {
+      const storeFbLoginStatus = JSON.parse(
+        localStorage.getItem("fbLoginStatus") || ""
+      );
+
+      const response: AxiosResponse = await axios.get(
+        `${ApiUrl}/${SdkVer}/me/accounts`,
+        {
+          params: {
+            access_token: storeFbLoginStatus.authResponse.accessToken,
+          },
+        }
+      );
+
+      if (response.status !== 200) {
+        return;
+      }
+
+      localStorage.setItem("fbPages", JSON.stringify(response.data.data));
+
+      return response.data.data;
+    } else {
+      return JSON.parse(storeFbPages);
+    }
+  } catch (error) {
+    console.log("[getMe] Error: ", error);
+  }
 };
 
 export const logout = () => {
-  // revoke app permissions to logout completely because FB.logout() doesn't remove FB cookie
-  window.FB.logout(function (response: string) {
-    checkLoginState();
-    // user is now logged out
-    console.log(response);
-    return (window.location.href = "/facebook/signin");
+  window.FB.getLoginStatus(function (response: any) {
+    if (response && response.status === "connected") {
+      window.FB.logout(function (response: any) {
+        localStorage.removeItem("igBusinessAccounts");
+        localStorage.removeItem("fbPages");
+        localStorage.removeItem("fbLoginStatus");
+        localStorage.removeItem("fbMe");
+        localStorage.removeItem("igAccounts");
+        return (window.location.href = "/signin/facebook");
+      });
+    }
   });
 };
